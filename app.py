81a10ad90cc28154593c27cc99334bb30bf88647
Sumit-Pathrabe <sumit.pathrabe17@gmail.com>
2026-05-26 14:33:40 +0530

    Added generator function

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ generator = pipeline(
     "text-generation",
     model="gpt2"
 )
+# Function to generate text
+def generate_story(prompt):
+
+    result = generator(
+        prompt,
+        max_length=120,
+        num_return_sequences=1,
+        temperature=0.8
+    )
+
+    print("\nGenerated Text:\n")
+    print(result[0]["generated_text"])
+    print("\n" + "="*60)
